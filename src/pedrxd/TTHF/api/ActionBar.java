b2bca2821e9dcaef0a6e14808183572bc26625e1
Pedro Ruiz <peruvapedro99@gmail.com>
2015-04-05 01:36:43 +0200

    Create ActionBar

--- src/pedrxd/TTHF/api/ActionBar.java
+++ src/pedrxd/TTHF/api/ActionBar.java
@@ -13,20 +13,31 @@ import org.bukkit.entity.Player;
 
 
 	public class ActionBar {
+		private String message = " ";
+		public ActionBar(){
+			
+		}
+		public void setMessage(String message){
+			this.message = message;
+		}
+		public String getMessage(){
+			return this.message;
+		}
+		public void broadcastMessage(){
+			for (Player list : Bukkit.getOnlinePlayers()) {
+				sendTo(list.getPlayer());
+			}
+		}
 
-		public static void sendActionBar(Player p, String message){
+		public void sendTo(Player p){
 			CraftPlayer craftplayer = (CraftPlayer) p;
 			IChatBaseComponent cbc = ChatSerializer.a("{\"text\": \"" + ChatColor.translateAlternateColorCodes('&', message.replaceAll("%player%", p.getName())) + "\"}");
 			PacketPlayOutChat packet = new PacketPlayOutChat(cbc, (byte) 2);
 			craftplayer.getHandle().playerConnection.sendPacket(packet);
 		}
-		public static void sendActionBar(String message){
 
-			for (Player list : Bukkit.getOnlinePlayers()) {
-				sendActionBar(list.getPlayer(), message);
-			}
-
-	    }
 		
-
+		
+		
+		
 }
